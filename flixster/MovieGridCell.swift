0e3bc5170dabe828afc0b0cbb77176ae78ffe701
MelaineSit <melaineist@gmail.com>
2021-02-27 00:28:26 -0500

    Part 2 Flix implemented

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Melaine SIt on 2/26/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
